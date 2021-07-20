f2f4d8bb243967c3651ebd8a3163544e85a0a871
Allen, Timothy <tallen@wharton.upenn.edu>
2021-07-20 08:48:40 -0400

    Update example to do both major and chromatic scales.

---
+++ examples/scales.py
@@ -0,0 +1,24 @@
+from midi import MIDIFile, Notes
+from midi.constants import *
+
+degrees = {}
+degrees["major"] = [C4, D4, E4, F4, G4, A4, B4, C5]
+degrees["chromatic"] = [C4, Cs4, D4, Ds4, E4, F4, Fs4, G4, Gs4, A4, As4, B4, C5]
+track = 0
+channel = 0
+time = 0  # In beats
+duration = 1  # In beats
+tempo = 60  # In BPM
+volume = 100  # 0-127, as per the MIDI standard
+
+for loop in ("major", "chromatic"):
+    MyMIDI = MIDIFile(1)  # One track
+    MyMIDI.add_tempo(track, time, tempo)
+
+    for i, pitch in enumerate(degrees[loop]):
+        MyMIDI.add_note(track, channel, pitch, time + i, duration, volume)
+
+    with open(f"{loop}-scale.mid", "wb") as output_file:
+        MyMIDI.write_file(output_file)
+
+print("The MIDI file has been written successfully.")
